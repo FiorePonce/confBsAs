f3f4848e5973c03e2da28325924efd36badea268
Fiorella <fioreponce09@gmail.com>
2022-10-27 12:22:14 -0300

    commit4

--- intJavaS.js
+++ intJavaS.js
@@ -1,2 +1,26 @@
 
- 
+const ticketValor=200
+
+const estudiante=80
+
+const trainee=50
+
+const junior=15
+
+function calcTotal() {
+    let cant=document.getElementById("cant").value
+    
+    let categoria=document.getElementById("catego").value
+
+    let total=0
+    if (categoria==1) {
+        total=cant*ticketValor*.2
+    } else if (categoria==2) {
+        total=cant*ticketValor*.5 
+    } else {
+        total=cant*ticketValor*.85
+    }
+
+    document.getElementById("total").innerHTML=`Total a pagar: $${total}`
+
+}
